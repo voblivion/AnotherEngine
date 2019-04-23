e8b87763b46f635bcf77458c5ac7117397a14abe
vob <voblivion@hotmail.fr>
2019-04-23 19:08:57 -0400

    Config - move export api macros to correct file

--- include/aoe/Config.h
+++ include/aoe/Config.h
@@ -83,13 +83,3 @@
 	#define AOE_API_IMPORT
 
 #endif
-
-#if defined(AOE_CORE_EXPORTS)
-
-	#define AOE_CORE_API AOE_API_EXPORT
-
-#else
-
-	#define AOE_CORE_API AOE_API_IMPORT
-
-#endif
--- include/aoe/core/Export.h
+++ include/aoe/core/Export.h
@@ -1,1 +1,11 @@
 #include <aoe/Config.h>
+
+#if defined(AOE_CORE_EXPORTS)
+
+#define AOE_CORE_API AOE_API_EXPORT
+
+#else
+
+#define AOE_CORE_API AOE_API_IMPORT
+
+#endif
