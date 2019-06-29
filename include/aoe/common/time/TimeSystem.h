aa83d45549132349ea48d262face95ce9f71803a
vob <voblivion@hotmail.fr>
2019-06-29 15:16:08 -0400

    TimeSystem fix - update is not virtual

--- include/aoe/common/time/TimeSystem.h
+++ include/aoe/common/time/TimeSystem.h
@@ -11,14 +11,13 @@ namespace aoe
 	namespace common
 	{
 		class AOE_COMMON_API TimeSystem final
-			: public ecs::ASystem
 		{
 		public:
 			// Constructors
 			explicit TimeSystem(ecs::WorldDataProvider& a_worldDataProvider);
 
 			// Methods
-			void update() const override;
+			void update() const;
 
 		private:
 			// Attributes
