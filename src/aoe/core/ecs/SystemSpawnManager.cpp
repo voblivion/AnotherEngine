8c58b7e622286bf73d3bf422d3909287c7ed80e9
vob <voblivion@hotmail.fr>
2019-06-29 15:01:12 -0400

    Missing spaces

--- src/aoe/core/ecs/SystemSpawnManager.cpp
+++ src/aoe/core/ecs/SystemSpawnManager.cpp
@@ -14,7 +14,7 @@ namespace aoe
 			ComponentManager const& a_componentManager)
 		{
 			auto const t_allocator = m_frameSpawns.get_allocator();
-			std::lock_guard<std::mutex> t_lock{m_mutex};
+			std::lock_guard<std::mutex> t_lock{ m_mutex };
 			m_frameSpawns.emplace_back(sta::allocatePolymorphic<Entity>(
 				t_allocator
 				, m_nextEntityId++, a_componentManager));
