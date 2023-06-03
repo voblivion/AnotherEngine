fe7bb4ad59e7299dbfc3e05224d1c83b25913c04
vob <voblivion@hotmail.fr>
2023-06-02 18:16:57 -0700

    remove main's references to old ecs framework

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@
 #include "vob/aoe/common/_render/Directorcomponent.h"
 #include "vob/aoe/common/physic/WorldPhysiccomponent.h"
 #include "vob/aoe/common/physic/DefaultDynamicsWorldHolder.h"
-#include "vob/aoe/ecs/world.h"
 #include "vob/aoe/common/physic/PhysicSystem.h"
 #include "vob/aoe/common/test/TestSystem.h"
 #include "vob/aoe/common/_render/RenderSystem.h"
@@ -346,9 +345,6 @@ char const* xinputMapping = "78696e70757401000000000000000000,XInput Gamepad (GL
 	",platform:Windows,a:b0,b:b1,x:b2,y:b3,leftshoulder:b4,rightshoulder:b5,back:b6,start:b7"
 	",leftstick:b8,rightstick:b9,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:a4"
 	",righttrigger:a5,dpup:h0.1,dpright:h0.2,dpdown:h0.4,dpleft:h0.8,";
-
-#include <vob/aoe/test.h>
-#include <vob/aoe/ecs/entity_map.h>
 
 int main()
 {
