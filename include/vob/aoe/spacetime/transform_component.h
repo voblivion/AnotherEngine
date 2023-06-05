cd8b5c7e9d5ac34faf74e89094296a202940ad62
vob <voblivion@hotmail.fr>
2023-06-05 00:35:15 -0700

    fix serialization of glm vectors for transform

--- include/vob/aoe/spacetime/transform_component.h
+++ include/vob/aoe/spacetime/transform_component.h
@@ -1,17 +1,19 @@
 #pragma once
 
+#include <vob/aoe/data/glm_accept.h>
+
+#include <vob/misc/std/message_macros.h>
 #include <vob/misc/visitor/macros.h>
 
 #include <glm/glm.hpp>
 #include <glm/ext/matrix_transform.hpp>
-
-#include <vob/aoe/data/glm_accept.h>
 
 
 namespace vob::aoest
 {
 	struct transform_component
 	{
+#pragma message(VOB_MISTD_TODO "should maybe split position / rotation into separate components.")
 		glm::mat4 m_matrix{ 1.0f };
 	};
 }
